5ddcd638afc93de02a04bd2797ffc978454ffd8a
HugoDelval <hugo.delval@insa-lyon.fr>
2015-11-18 11:02:56 +0100

    suppression des erreurs, on peut tester plan et intersection (sauf chargerPlan)

--- src/main/java/optimod/modele/Plan.java
+++ src/main/java/optimod/modele/Plan.java
@@ -4,10 +4,12 @@ import optimod.modele.Intersection;
 
 import java.util.*;
 
+
 /**
  * 
  */
 public class Plan {
+
 
     /**
      * Default constructor
@@ -30,12 +32,13 @@ public class Plan {
     }
 
     /**
-     * @param adresse
+     * @param adresse l'identifiant où trouver l'intersection
+     * @return l'intersection correspondant à l'adresse
      */
     public Intersection trouverIntersection(int adresse) {
         Intersection intersectionTrouvee = null;
         for(Intersection inter: intersections) {
-            if (inter.adresse == adresse) {
+            if (inter.getAdresse() == adresse) {
                 intersectionTrouvee = inter;
                 break;
             }
@@ -45,14 +48,15 @@ public class Plan {
 
     /**
      * contrat : trouve la 1ere intersection dans le cercle, même si il y en a plusieurs (attention au radius trop grand)
-     * @param x 
-     * @param y 
-     * @param radius
+     * @param x la coordonnée x du cercle dans lequel trouver l'intersection
+     * @param y la coordonnée y du cercle dans lequel trouver l'intersection
+     * @param rayon le rayon du cercle dans lequel trouver l'intersection
+     * @return la 1ere intersection dans le cercle, même si il y en a plusieurs
      */
-    public Intersection trouverIntersection(int x, int y, int radius) {
+    public Intersection trouverIntersection(int x, int y, int rayon) {
         Intersection intersectionTrouvee = null;
         for(Intersection inter: intersections) {
-            if ( Math.pow(inter.x - x, 2) + Math.pow(inter.y - y, 2) <= radius*radius ) {
+            if ( inter.estLocalisee(x, y, rayon) ) {
                 intersectionTrouvee = inter;
                 break;
             }
