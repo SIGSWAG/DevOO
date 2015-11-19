547ab399489b9e92439906df12caf16a9640a204
Loïc Touzard <loic.touzard@gmail.com>
2015-11-19 19:32:26 +0100

    Avancement méthode construirePlanAPartirDeDOMXML

--- src/main/java/optimod/es/xml/DeserialiseurXML.java
+++ src/main/java/optimod/es/xml/DeserialiseurXML.java
@@ -2,6 +2,7 @@ package optimod.es.xml;
 
 import optimod.modele.Intersection;
 import optimod.modele.Plan;
+import optimod.modele.Troncon;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -12,10 +13,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Loïc Touzard on 18/11/2015.
@@ -56,39 +54,54 @@ public class DeserialiseurXML { // Singleton
         // Parcours de tous les Noeud (Intersections)
         NodeList listeNoeuds = noeudDOMRacine.getElementsByTagName("Noeud");
         Map<Integer, Intersection> intersections = new HashMap();
+        Map<Integer, Element> noeudsListe = new HashMap();
+
+        // Récupération de toutes les intersections
         for (int i = 0; i < listeNoeuds.getLength(); i++) {
+            Element noeud = (Element) listeNoeuds.item(i);
+
             // Validation des attributs
-            int adresse = Integer.parseInt(listeNoeuds.item(i).getAttributes().getNamedItem("id").getNodeValue());
+            int adresse = Integer.parseInt(noeud.getAttribute("id"));
             if (adresse <= 0)
                 throw new ExceptionXML("Erreur lors de la lecture du fichier : l'ID d'un Noeud doit être positif");
-            int x = Integer.parseInt(listeNoeuds.item(i).getAttributes().getNamedItem("x").getNodeValue());
+            int x = Integer.parseInt(noeud.getAttribute("x"));
             if (x <= 0)
-                throw new ExceptionXML("Erreur lors de la lecture du fichier : La coordonnée x doit être positive");
-            int y = Integer.parseInt(listeNoeuds.item(i).getAttributes().getNamedItem("y").getNodeValue());
+                throw new ExceptionXML("Erreur lors de la lecture du fichier : La coordonnée x d'un Noeud doit être positive");
+            int y = Integer.parseInt(noeud.getAttribute("y"));
             if (y <= 0)
-                throw new ExceptionXML("Erreur lors de la lecture du fichier : La coordonnée y doit être positive");
+                throw new ExceptionXML("Erreur lors de la lecture du fichier : La coordonnée y d'un Noeud doit être positive");
 
-            intersections.put(adresse,new Intersection(x,y,adresse));
+            // Création de l'intersection
+            intersections.put(adresse, new Intersection(x, y, adresse));
+            noeudsListe.put(adresse, noeud);
         }
 
-        NodeList listeNoeud = noeudDOMRacine.getElementsByTagName("Noeud");
-        Map<Integer, Intersection> intersections = new HashMap();
+        // Pour chaque intersection on créée ses tronçonsSortants
+        for (Map.Entry<Integer, Intersection> paire : intersections.entrySet())
+        {
+            Intersection intersection = paire.getValue();
 
+            NodeList listeTroncons = noeudsListe.get(paire.getKey()).getElementsByTagName("LeTronconSortant");
+            for (int j = 0; j < listeTroncons.getLength(); j++){
+                Element leTronconSortant = (Element) listeNoeuds.item(j);
 
+                // Validation des attributs
+                String nomRue = leTronconSortant.getAttribute("nomRue");
+                if (nomRue.isEmpty())
+                    throw new ExceptionXML("Erreur lors de la lecture du fichier : Le nom d'un leTronconSortant doit être renseigné");
+                double vitesse = Double.parseDouble(leTronconSortant.getAttribute("vitesse"));
+                if (vitesse < 0)
+                    throw new ExceptionXML("Erreur lors de la lecture du fichier : La vitesse d'un leTronconSortant doit être positive");
+                double longueur = Double.parseDouble(leTronconSortant.getAttribute("longueur"));
+                if (longueur < 0)
+                    throw new ExceptionXML("Erreur lors de la lecture du fichier : La longueur d'un leTronconSortant doit être positive");
+                int idNoeudDestination = Integer.parseInt(leTronconSortant.getAttribute("idNoeudDestination"));
+                Intersection intersectionDestination = intersections.get(idNoeudDestination);
+                if(intersectionDestination == null)
+                    throw new ExceptionXML("Erreur lors de la lecture du fichier : Un leTronconSortant possède un NoeudDestination inconnu");
 
-
-
-        int largeur = Integer.parseInt(noeudDOMRacine.getAttribute("largeur"));
-        if (largeur <= 0)
-            throw new ExceptionXML("Erreur lors de la lecture du fichier : La largeur du plan doit etre positive");
-        plan.reset(largeur,hauteur);
-        NodeList listeCercles = noeudDOMRacine.getElementsByTagName("cercle");
-        for (int i = 0; i < listeCercles.getLength(); i++) {
-            plan.ajoute(creeCercle((Element) listeCercles.item(i)));
-        }
-        NodeList listeRectangles = noeudDOMRacine.getElementsByTagName("rectangle");
-        for (int i = 0; i < listeRectangles.getLength(); i++) {
-            plan.ajoute(creeRectangle((Element) listeRectangles.item(i)));
+                // Création du troncon
+            }
         }
     }
 }
