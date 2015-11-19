b9ac1163563aa8fc89293c283ad5b2582600df18
HugoDelval <hugo.delval@insa-lyon.fr>
2015-11-19 19:42:18 +0100

    mise à jour Troncon : longueur, duree, vitesse

--- src/main/java/optimod/modele/Troncon.java
+++ src/main/java/optimod/modele/Troncon.java
@@ -2,11 +2,18 @@ package optimod.modele;
 
 public class Troncon {
 
-    public Troncon(Intersection intersection, double vitesse, double longueur) {
+    public Troncon(Intersection intersection, double vitesse, double longueur, String nom) {
         this.duree = (int) (longueur / vitesse);
         this.arrivee = intersection;
+        this.nom = nom;
+        this.longueur = longueur;
+        this.vitesse = vitesse;
     }
 
+
+    private double longueur;
+
+    private double vitesse;
 
     private int duree;
 
@@ -18,15 +25,23 @@ public class Troncon {
         return duree;
     }
 
-    public void setDuree(int duree) {
-        this.duree = duree;
-    }
-
     public Intersection getArrivee() {
         return arrivee;
     }
 
     public void setArrivee(Intersection arrivee) {
         this.arrivee = arrivee;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public double getLongueur() {
+        return longueur;
+    }
+
+    public double getVitesse() {
+        return vitesse;
     }
 }
