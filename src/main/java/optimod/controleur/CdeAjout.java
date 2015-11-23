f2c925ee7704a0d10c97a2e553015e58eefd282c
Loïc Touzard <ltouzard@degustandco.com>
2015-11-23 09:57:35 +0100

    Debut implementation du pattern Commande

---
+++ src/main/java/optimod/controleur/CdeAjout.java
@@ -0,0 +1,32 @@
+package optimod.controleur;
+
+import optimod.modele.*;
+
+/**
+ * Created by (PRO) Loïc Touzard on 23/11/2015.
+ */
+public class CdeAjout implements Commande{
+    private Ordonnanceur ordonnanceur;
+    private Livraison livraison;
+    private Intersection intersection;
+
+    /**
+     * Cree la commande qui ajoute une Intersection i avant la Livraison l à la DemandeLivraison
+     * @param o
+     * @param l
+     * @param i
+     */
+    public CdeAjout(Ordonnanceur o, Livraison l, Intersection i){
+        this.ordonnanceur = o;
+        this.livraison = l;
+        this.intersection = i;
+    }
+
+    public void doCde() {
+        ordonnanceur.ajouterLivraison(intersection, livraison);
+    }
+
+    public void undoCde() {
+        ordonnanceur.supprimerLivraison(livraison);
+    }
+}
---
+++ src/main/java/optimod/controleur/CdeEchange.java
@@ -0,0 +1,7 @@
+package optimod.controleur;
+
+/**
+ * Created by (PRO) Loïc Touzard on 23/11/2015.
+ */
+public class CdeEchange {
+}
