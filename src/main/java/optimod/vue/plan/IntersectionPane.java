88e9d238a4e3a38ab836118ad39b2b01f1d9fb3a
Aurélien Coussat <aurelien.coussat@insa-lyon.fr>
2015-11-24 16:56:30 +0100

    Infobulles sur les intersections avec leurs informations

--- src/main/java/optimod/vue/plan/IntersectionPane.java
+++ src/main/java/optimod/vue/plan/IntersectionPane.java
@@ -1,8 +1,16 @@
 package optimod.vue.plan;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.EventHandler;
+import javafx.scene.control.Tooltip;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 import optimod.modele.Intersection;
+
+import java.lang.reflect.Field;
 
 /**
  * Représente une intersection à l'écran.
@@ -14,14 +22,68 @@ public class IntersectionPane extends Circle {
 
     public static final Color COULEUR_ENTREPOT = Color.GREEN;
     public static final Color COULEUR_DEFAUT = Color.BLACK;
+    public static final Color COULEUR_SURVOL = Color.BLUE;
 
     private Intersection intersection;
     private boolean estEntrepot;
+    private boolean survol;
+
+    private Tooltip infobulle;
 
     public IntersectionPane(Intersection intersection) {
         super(intersection.getX(), intersection.getY(), TAILLE);
+
         this.intersection = intersection;
+
         setEstEntrepot(false);
+        survol = false;
+
+        infobulle = new Tooltip(getTexteInfobulle());
+        dureeApparition(infobulle, 25);
+
+        this.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+                survol();
+            }
+        });
+        this.setOnMouseExited(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+                quitteSurvol();
+            }
+        });
+    }
+
+    public void setEstEntrepot(boolean estEntrepot) {
+        this.estEntrepot = estEntrepot;
+        colorier();
+    }
+
+    private void survol() {
+        survol = true;
+        Tooltip.install(this, infobulle);
+        colorier();
+    }
+
+    private void quitteSurvol() {
+        survol = false;
+        Tooltip.uninstall(this, infobulle);
+        colorier();
+    }
+
+    private void colorier() {
+        if (survol)
+            setFill(COULEUR_SURVOL);
+        else if (estEntrepot)
+            setFill(COULEUR_ENTREPOT);
+        else
+            setFill(COULEUR_DEFAUT);
+    }
+
+    private String getTexteInfobulle() {
+        return String.format("(%s;%s)\nAdresse : %s",
+                intersection.getX(),
+                intersection.getY(),
+                intersection.getAdresse());
     }
 
     public Intersection getIntersection() {
@@ -36,12 +98,29 @@ public class IntersectionPane extends Circle {
         return intersection.getY();
     }
 
-    public void setEstEntrepot(boolean estEntrepot) {
-        this.estEntrepot = estEntrepot;
-        if (estEntrepot)
-            setFill(COULEUR_ENTREPOT);
-        else
-            setFill(COULEUR_DEFAUT);
+    /**
+     * Hack permettant de personnaliser la durée d'apparition des infobulles.
+     * <p>
+     * Source : http://stackoverflow.com/a/27739605
+     *
+     * @param tooltip
+     */
+    private static void dureeApparition(Tooltip tooltip, int duree) {
+        try {
+            Field fieldBehavior = tooltip.getClass().getDeclaredField("BEHAVIOR");
+            fieldBehavior.setAccessible(true);
+            Object objBehavior = fieldBehavior.get(tooltip);
+
+            Field fieldTimer = objBehavior.getClass().getDeclaredField("activationTimer");
+            fieldTimer.setAccessible(true);
+            Timeline objTimer = (Timeline) fieldTimer.get(objBehavior);
+
+            objTimer.getKeyFrames().clear();
+            objTimer.getKeyFrames().add(new KeyFrame(new Duration(duree)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
